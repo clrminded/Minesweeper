9caec0e8e36148147edb563a4db41c5744879720
clr <the_rocket_scientist@icloud.com>
2023-10-04 22:08:04 -0700

    adding bare files to start project

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Minesweeper</title>
+		<script type="text/javascript" src="function.js"></script>
+		<script type="text/javascript" src="class.js"></script>
+	</head>
+	
+	<body onload="avoid_image_loading_delay()">
+		<button id="start">Start</button>&nbsp;&nbsp;&nbsp;&nbsp;
+		<span id="game_over" style="display: none">Game Over</span>
+		<br/><br/>
+		<script>
+		<!-- PLACE YOUR IMPLEMENTATION BELOW -->
+
+		
+		<!-- PLACE YOUR IMPLEMENTATION ABOVE -->
+		</script>
+	</body>
+</html>
