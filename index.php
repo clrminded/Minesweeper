771c9afff8267996a64f2c07af74faa8eab52487
Christopher Liudahl-Rackley <the_rocket_scientist@icloud.com>
2023-12-17 22:30:08 -0800

    update index

--- index.php
+++ index.php
@@ -10,27 +10,22 @@
 		<span id="game_over" style="display: none">Game Over</span>
 		<br/><br/>
 		<script>
-		<!-- PLACE YOUR IMPLEMENTATION BELOW -->
+		//<!-- PLACE YOUR IMPLEMENTATION BELOW -->
 			
 			// create a div with id=container, then add styling and add it to the body.
 			let container = document.createElement("div");
 			container.setAttribute("id", "container");
 			document.body.appendChild(container);
 				
-				
+			
 			// assign variable names game to Minesweeper object and add it to body
 			// Look in class.js to see the implementation of Minesweeper()
 			let game = new Minesweeper();
-			game.init_board();
+
+			const start_button = document.getElementById("start");
+			start_button.onclick = (event) => game.init_board(event);
 			
-			for(let i = 0; i < game.rows; i++) {
-				for(let j = 0; j < game.columns; j++) {
-					create_button();
-				}
-				create_line_break();
-			}
-		
-		<!-- PLACE YOUR IMPLEMENTATION ABOVE -->
+		//<!-- PLACE YOUR IMPLEMENTATION ABOVE -->
 		</script>
 	</body>
 </html>
